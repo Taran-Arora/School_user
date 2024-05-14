65907212bf3fc105aa1aa7a41c963aa3e3a47ef2
taran <tp9014361@gmail.com>
2024-05-14 16:58:27 +0530

    text

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,24 +1,56 @@
 import React from 'react'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import _fetch from '../../src/config/api';
+import { api_url } from '../../src/config/config';
+import toasted from '../../src/config/toast';
 
 export default function Login() {
+    const navigate = useNavigate();
+
+    const [allFields, setAllFields] = useState({
+        'username': '',
+        'password': ''
+      });
+
+      const inputFieldsData = (event) => {
+        setAllFields({
+          ...allFields,
+          [event.target.name]: event.target.value
+        })
+      }
+
+      const loginSubmit = async () => {
+
+        let res = await _fetch(`${api_url}admin/login/`, 'POST', allFields, {});
+    
+        if (res?.status == 200) {
+          toasted.success(res?.message);
+          navigate('/');
+        }
+        else if (res?.message) {
+          toasted.error(res?.message);
+        }
+      }
+
     return (
         <div>
             <div className="container">
                 <div className="login_box">
                     <h1>Login</h1>
                     <div className="input_box">
-                        <input type="text" required />
+                        <input type="text" required name='username' onChange={inputFieldsData} value={allFields?.username}  />
                         <label htmlFor="">Email</label>
                         <ion-icon className="icon" name="mail-outline"></ion-icon>
                     </div>
                     <div className="input_box">
-                        <input type="password" required />
+                        <input type="password" required value={allFields?.password} name='password' onChange={inputFieldsData}/>
                         <label htmlFor="">password</label>
                         <ion-icon className="icon" name="lock-closed-outline"></ion-icon>
                     </div>
      
                     <div className="login">
-                        <button>Login</button>
+                        <button type='button' onClick={loginSubmit}>Login</button>
                     </div>
                     <div>
 
---
+++ src/config/config.js
@@ -0,0 +1,15 @@
+// import site_logo from '../Assets/image/logo.png';
+// export const logo = site_logo;
+export const title = 'ecommerce';
+export const website = ''; 
+export const shortTitle = 'ecommerce';
+export const titleDescription = 'ecommerce-site';
+
+export const api_url = 'http://192.168.29.157:8001/api/';
+
+export const short_api_url = 'http://192.168.29.15:8000/tokenImage/';
+
+
+
+
+
