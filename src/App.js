84be08dc746ce0d854a761c2c118840364a82107
Sachin Mittal <skmittal829@gmail.com>
2024-05-17 15:24:18 +0530

    bbb

--- src/App.js
+++ src/App.js
@@ -4,22 +4,46 @@ import Login from './Components/Login';
 import Links from './Components/Links';
 import Register from './Components/Register';
 import Forgot from './Components/Forget';
+// import 'bootstrap-icons/font/fonts/boostrap-icons.css'
 import './Style/Style.scss';
-import { LinkSharp } from '@mui/icons-material';
+import Sidebar from './Components/Sidebar';
+import Home from './Components/Home';
+import { useState } from 'react';
 
 function App() {
+
+  const [toggle, setToggle] = useState(true)
+  const Toggle = () => {
+    setToggle(!toggle)
+  }
+
   return (
     <>
-      <BrowserRouter>
-
+      {/* <BrowserRouter>
+      <div className="container-fluid">
+      <Sidebar/>
+      </div>
         <Routes>
-          <Route path="/"/>
+          <Route path="/" />
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Links />} />
           <Route path="/register" element={<Register />} />
           <Route path="/forgot" element={<Forgot />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+
+      <div className="container-fluid bg-secondary min-vh-100">
+        <div className="row">
+          {toggle && <div className="col-4 col-md-2 bg-white vh-100 position-fixed">
+            <Sidebar />
+          </div>}
+          {toggle && <div className="col-4 col-md-2"></div> }
+          <div className="col">
+            <Home Toggle={Toggle} />
+          </div>
+        </div>
+      </div>
+
 
     </>
 
---
+++ src/Components/Home.js
@@ -0,0 +1,123 @@
+import React from 'react'
+import Nav from './Nav'
+
+const Home = ({Toggle}) => {
+    return (
+        <div className='px-3'>
+            <Nav Toggle={Toggle}/>
+            <div className="container-fluid">
+                <div className="row g-3 my-2">
+                    <div className="col-md-3 p-1">
+                        <div className="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                            <div>
+                                <h3 className='fs-2'>15000</h3>
+                                <p className='fs-5'>Students</p>
+                            </div>
+                            <i></i>
+                        </div>
+                    </div>
+                    <div className="col-md-3 p-1">
+                        <div className="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                            <div>
+                                <h3 className='fs-2'>2250</h3>
+                                <p className='fs-5'>Teachers</p>
+                            </div>
+                            <i></i>
+                        </div>
+                    </div>
+                    <div className="col-md-3 p-1">
+                        <div className="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                            <div>
+                                <h3 className='fs-2'>5900</h3>
+                                <p className='fs-5'>Parents</p>
+                            </div>
+                            <i></i>
+                        </div>
+                    </div>
+                    <div className="col-md-3 p-1">
+                        <div className="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                            <div>
+                                <h3 className='fs-2'>$232323</h3>
+                                <p className='fs-5'>Earning</p>
+                            </div>
+                            <i></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <table className="table mt-5 mx-4">
+                <thead className='my-3'><h3>All Schools</h3></thead>
+                <tbody>
+                    <tr>
+                        <th scope="col">S. No.</th>
+                        <th scope="col">School Name</th>
+                        <th scope="col">School Email</th>
+                        <th scope="col"></th>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>School@gmail.com</td>
+                        <td className='d-flex justify-content-evenly'>
+                            <a href="" className='btn btn-primary'>Login</a>
+                            <a href="" className='btn btn-primary'>View</a>
+                            <a href="" className='btn btn-primary'>Block</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td className='d-flex justify-content-evenly'>
+                            <a href="" className='btn btn-primary'>Login</a>
+                            <a href="" className='btn btn-primary'>View</a>
+                            <a href="" className='btn btn-primary'>Block</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Larry the Bird</td>
+                        <td>School@gmail.com</td>
+                        <td className='d-flex justify-content-evenly'>
+                            <a href="" className='btn btn-primary'>Login</a>
+                            <a href="" className='btn btn-primary'>View</a>
+                            <a href="" className='btn btn-primary'>Block</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">4</th>
+                        <td>Larry the Bird</td>
+                        <td>School@gmail.com</td>
+                        <td className='d-flex justify-content-evenly'>
+                            <a href="" className='btn btn-primary'>Login</a>
+                            <a href="" className='btn btn-primary'>View</a>
+                            <a href="" className='btn btn-primary'>Block</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">5</th>
+                        <td>Larry the Bird</td>
+                        <td>School@gmail.com</td>
+                        <td className='d-flex justify-content-evenly'>
+                            <a href="" className='btn btn-primary'>Login</a>
+                            <a href="" className='btn btn-primary'>View</a>
+                            <a href="" className='btn btn-primary'>Block</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">6</th>
+                        <td>Larry the Bird</td>
+                        <td>School@gmail.com</td>
+                        <td className='d-flex justify-content-evenly'>
+                            <a href="" className='btn btn-primary'>Login</a>
+                            <a href="" className='btn btn-primary'>View</a>
+                            <a href="" className='btn btn-primary'>Block</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Home
