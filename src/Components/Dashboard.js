0532efeb8ce53bd4c7a0306705010abafb05acc2
Sachin Mittal <skmittal829@gmail.com>
2024-05-18 12:31:21 +0530

    samrat bhai

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,16 +1,24 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 import Home from './Home';
 import Sidebar from './Sidebar';
 
 const Dashboard = () => {
-    const [toggle, setToggle] = useState(true)
+    const [toggle, setToggle] = useState(true);
+
+    useEffect(() => {
+        if (window.innerWidth < 1200) {
+            setToggle(false);
+        }
+    }, []);
+
     const Toggle = () => {
-        setToggle(!toggle)
-    }
+        setToggle(!toggle);
+    };
+
     return (
         <div className="container-fluid dash min-vh-100">
             <div className="row">
-                {toggle && <div className="col-4 col-md-2 bg-white vh-100 position-fixed">
+                {toggle && <div className="col-10 col-md-4 col-lg-3 col-xl-2 bg-white vh-100 position-fixed z-1">
                     <Sidebar />
                 </div>}
                 {toggle && <div className="col-4 col-md-2"></div>}
