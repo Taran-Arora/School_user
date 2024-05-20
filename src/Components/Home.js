1f6170ac1aaede02c532f6ecf45c98f1c0fde663
taran <tp9014361@gmail.com>
2024-05-20 14:26:42 +0530

    tegt

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Nav from './Nav'
 import Students from '../assets/Images/graduation.png'
 import Parents from '../assets/Images/parents.png'
@@ -9,12 +9,13 @@ import _fetch from '../../src/config/api';
 import { api_url } from '../../src/config/config';
 // import { useState } from 'react'
 
-const Home = ({Toggle}) => {
+const Home = ({ Toggle }) => {
     const Navigate = useNavigate();
 
-    // const [schoolData, setschoolData] = useState([]);
+    const [totalUsers, setTotalUsers] = useState({total_users: '', active_users: '' });
     const [allData, setAllData] = useState([]);
     const [token, settoken] = useState([]);
+    console.log('totalUsers', totalUsers);
     useEffect(() => {
         const storedschool = localStorage.getItem('token');
         if (storedschool) {
@@ -29,10 +30,12 @@ const Home = ({Toggle}) => {
         if (token) {
             res = await _fetch(`${api_url}userdata/`, 'GET', {}, {});
         }
-        console.log('res', res);
         if (res?.status === 200) {
-            console.log(res);
-            setAllData(res?.data);
+            setAllData(res?.users);
+            setTotalUsers({
+                'total_users': res?.total_users,
+                'active_users': res?.active_users
+            });
         }
         else {
             Navigate('/login');
@@ -41,67 +44,75 @@ const Home = ({Toggle}) => {
 
     return (
         <div className='px-3'>
-            <Nav Toggle={Toggle}/>
+            <Nav Toggle={Toggle} />
             <div className="container-fluid">
                 <div className="row g-3 my-2">
                     <div className="col-md-3 p-1">
                         <div className="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
                             <div>
-                                <h3 className='fs-2'></h3>
-                                <p className='fs-5'>Students</p>
+                                <h3 className='fs-2'>{totalUsers?.total_users}</h3>
+                                <p className='fs-5'>Total Users</p>
                             </div>
-                            <img src={Students} alt="" className='img-fluid'/>
+                            <img src={Students} alt="" className='img-fluid' />
                         </div>
                     </div>
                     <div className="col-md-3 p-1">
                         <div className="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
                             <div>
-                                <h3 className='fs-2'>2250</h3>
-                                <p className='fs-5'>Teachers</p>
+                                <h3 className='fs-2'>{totalUsers?.active_users}</h3>
+                                <p className='fs-5'>Is Active Users</p>
                             </div>
-                            <img src={Teacher} alt="" className='img-fluid'/>
+                            <img src={Teacher} alt="" className='img-fluid' />
                         </div>
                     </div>
                     <div className="col-md-3 p-1">
                         <div className="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
                             <div>
-                                <h3 className='fs-2'>5900</h3>
-                                <p className='fs-5'>Parents</p>
+                                <h3 className='fs-2'>0</h3>
+                                <p className='fs-5'>In Active Users</p>
                             </div>
-                            <img src={Parents} alt="" className='img-fluid'/>
+                            <img src={Parents} alt="" className='img-fluid' />
                         </div>
                     </div>
                     <div className="col-md-3 p-1">
                         <div className="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
                             <div>
-                                <h3 className='fs-2'>$232323</h3>
-                                <p className='fs-5'>Earning</p>
+                                <h3 className='fs-2'>1</h3>
+                                <p className='fs-5'>Blocked Users</p>
                             </div>
-                            <img src={Earning} alt="" className='img-fluid'/>
+                            <img src={Earning} alt="" className='img-fluid' />
                         </div>
                     </div>
                 </div>
             </div>
             <table className="table mt-5 mx-4">
-                <thead className='my-3'><h3>All Schools</h3></thead>
-                <tbody>
+                <thead className='my-3'><h3>All Schools</h3>
                     <tr>
                         <th scope="col">S. No.</th>
                         <th scope="col">School Name</th>
                         <th scope="col">School Email</th>
                         <th scope="col"></th>
                     </tr>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>School@gmail.com</td>
-                        <td className='d-flex justify-content-evenly'>
-                            <a href="" className='btn btn-primary'>Login</a>
-                            <a href="" className='btn btn-primary'>View</a>
-                            <a href="" className='btn btn-primary'>Block</a>
-                        </td>
-                    </tr>
-                    <tr>
+                </thead>
+                <tbody>
+                    {allData?.map((item, index) => {
+                        return (
+                            <>
+
+                                <tr>
+                                    <th scope="row">{index + 1}</th>
+                                    <td>{item?.first_name}</td>
+                                    <td>{item?.username}</td>
+                                    <td className='d-flex justify-content-evenly'>
+                                        <a href="" className='btn btn-primary'>Login</a>
+                                        <a href="" className='btn btn-primary'>View</a>
+                                        <a href="" className='btn btn-primary'>Block</a>
+                                    </td>
+                                </tr>
+                            </>
+                        )
+                    })}
+                    {/* <tr>
                         <th scope="row">2</th>
                         <td>Jacob</td>
                         <td>Thornton</td>
@@ -150,7 +161,7 @@ const Home = ({Toggle}) => {
                             <a href="" className='btn btn-primary'>View</a>
                             <a href="" className='btn btn-primary'>Block</a>
                         </td>
-                    </tr>
+                    </tr> */}
                 </tbody>
             </table>
         </div>
