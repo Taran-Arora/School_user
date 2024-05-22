d80cedb987fcab2c3f5e44e7adf0062937f6072e
sonjeet-kaur <gnisonjeet@gmail.com>
2024-05-22 12:53:19 +0530

    commit

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -18,6 +18,28 @@ import toasted from '../config/toast';
 const Sidebar = () => {
 
   const navigate = useNavigate();
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token');
+    if(storedToken) {
+      setToken(storedToken);
+      schoolData();
+    }
+  }, [token]);
+
+  const schoolData = async () => {
+    if (!token) {
+      return;
+    }
+    const res = await _fetch(`${api_url}userdata/`, 'GET', {}, {});
+
+    if (res?.status === 200) {
+
+    } else {
+      navigate('/login');
+    }
+  };
 
   const logOut = async () => {
 
@@ -38,47 +60,47 @@ const Sidebar = () => {
   return (
     <>
       <div className='bg-white sidebar p-2 max-vh-100'>
-      <div className='m-2 d-flex align-items-center'>
-        <PersonIcon className='me-2 fs-4'></PersonIcon>
-        <span className='brand-name fs-4'>Admin</span>
-      </div>
-      <hr className='text-dark'/>
-      <div className='list-group list-group-flush max-vh-100'>
-        <Link to="/" className="list-group-item py-2 d-flex align-items-center my-2">
+        <div className='m-2 d-flex align-items-center'>
+          <PersonIcon className='me-2 fs-4'></PersonIcon>
+          <span className='brand-name fs-4'>Admin</span>
+        </div>
+        <hr className='text-dark' />
+        <div className='list-group list-group-flush max-vh-100'>
+          <Link to="/" className="list-group-item py-2 d-flex align-items-center my-2">
             <GridViewIcon className='fs-5 me-2'></GridViewIcon>
             <span className='fs-5'>DashBoard</span>
-        </Link>
-        <Link to="/allschools" className="list-group-item py-2 d-flex align-items-center my-2">
+          </Link>
+          <Link to="/allschools" className="list-group-item py-2 d-flex align-items-center my-2">
             <GridViewIcon className='fs-5 me-2'></GridViewIcon>
             <span className='fs-5'>All Schools</span>
-        </Link>
-        <Link to="/allteachers" className="list-group-item py-2 d-flex align-items-center my-2">
+          </Link>
+          <Link to="/allteachers" className="list-group-item py-2 d-flex align-items-center my-2">
             <SchoolIcon className='fs-4 me-2'></SchoolIcon>
             <span className='fs-5'>All Teachers</span>
-        </Link>
-        <Link to="/allstudents" className="list-group-item py-2 d-flex align-items-center my-2">
+          </Link>
+          <Link to="/allstudents" className="list-group-item py-2 d-flex align-items-center my-2">
             <SchoolIcon className='fs-4 me-2'></SchoolIcon>
             <span className='fs-5'>All Students</span>
-        </Link>
-        
-        <Link to="/" className="list-group-item py-2 d-flex align-items-center my-2">
+          </Link>
+
+          <Link to="/" className="list-group-item py-2 d-flex align-items-center my-2">
             <ManageAccountsIcon className='fs-4 me-2'></ManageAccountsIcon>
             <span className='fs-5'>My Account</span>
-        </Link>
-        {/* <Link to="/login" className="list-group-item py-2 position-absolute bottom-0 d-flex align-items-center logout-btn">
+          </Link>
+          {/* <Link to="/login" className="list-group-item py-2 position-absolute bottom-0 d-flex align-items-center logout-btn">
             <LogoutIcon className='fs-4 me-2'></LogoutIcon>
             <span className='fs-5'>Logout</span>
         </Link> */}
-        <Button type='button' onClick={logOut}>
-          Logout
-        </Button>
+          <Button type='button' onClick={logOut}>
+            Logout
+          </Button>
+        </div>
       </div>
-    </div>
     </>
-   
 
 
-  
+
+
   )
 }
 
