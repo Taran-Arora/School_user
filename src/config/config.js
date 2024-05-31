7b77a486b778e7ea92c84e5cffcfa591328f5b7b
taran <tp9014361@gmail.com>
2024-05-31 12:21:21 +0530

    tyt

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,7 @@ export const website = '';
 export const shortTitle = 'ecommerce';
 export const titleDescription = 'ecommerce-site';
 
-export const api_url = 'http://192.168.29.159:8001/api/';
+export const api_url = 'http://192.168.29.157:8000/api/';
 
 
 
