3c5a6d3e5ebe8bdaa9ab30ca03cc74fb8c36fd00
Sachin Mittal <skmittal829@gmail.com>
2024-06-12 10:17:43 +0530

    xd

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
       <ToggleProvider>
         <BrowserRouter>
           <Routes>
-            {/* <Route path="/" element={<Login />} /> */}
+            <Route path="/" element={<Login />} />
             <Route path="/forgot" element={<Forgot />} />
 
             <Route element={<PrivateRoutes />}>
