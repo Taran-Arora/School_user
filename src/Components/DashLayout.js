10140ffd3ce58d4b954c986d3b3e2406a8d7125b
Sachin Mittal <skmittal829@gmail.com>
2024-06-17 16:39:32 +0530

    new

--- src/Components/DashLayout.js
+++ src/Components/DashLayout.js
@@ -11,7 +11,7 @@ export default function DashLayout() {
         <Container fluid className="dash min-vh-100">
             <div className="row">
                 <div
-                    className={`col-12 col-md-4 col-lg-3 col-xl-2  vh-100 position-fixed z-1 sidebar ${toggle ? 'sidebar-visible' : 'sidebar-hidden'}`}
+                    className={`col-12 col-md-4 col-lg-3 col-xl-2  vh-100 position-fixed sidebar ${toggle ? 'sidebar-visible' : 'sidebar-hidden'}`}
                     style={{ transition: 'transform 0.5s ease', transform: toggle ? 'translateX(0)' : 'translateX(-100%)' }}
                 >
                     <Sidebar />
